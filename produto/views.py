1f5f50360a3d36587d3b0113db8a2e80e695a5b9
imHireki <imhireki@gmail.com>
2021-07-09 16:18:54 -0300

    remove images with the object

--- produto/views.py
+++ produto/views.py
@@ -7,3 +7,12 @@ from .serializers import ProdutoSerializer
 class ProdutoViewSet(viewsets.ModelViewSet):
     queryset = Produto.objects.all() 
     serializer_class = ProdutoSerializer 
+
+    def destroy(self, request, pk, *args, **kwargs):
+        # Getting the right object from queryset
+        produto = self.get_queryset().filter(id=pk).first()
+        # Destroying associated image when DELETE is called
+        if produto.imagem:
+            produto.imagem.delete(save=True)
+        
+        return super().destroy(request, *args, **kwargs)
