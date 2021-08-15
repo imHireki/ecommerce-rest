9e9d03f9e09f866af2fdecf59071894edeaa2dd1
imHireki <imhireki@gmail.com>
2021-08-15 12:17:51 -0300

    restart project

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""loja URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
@@ -14,19 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
-from produto.views import ProdutoViewSet
-from django.conf import settings
-from django.conf.urls.static import static
-
-router = routers.DefaultRouter()
-router.register(r'produtos', ProdutoViewSet)
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    # Rest
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
--- produto/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Produto, Variacao
-
-
-class ProdutoSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Produto
-        fields = '__all__'
