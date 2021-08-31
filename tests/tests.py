a5a991822c5c5b4e91eb158911b1e55f3d12d8ab
imHireki <imhireki@gmail.com>
2021-08-31 17:21:40 -0300

    add test for the images

--- tests/tests.py
+++ tests/tests.py
@@ -1,62 +1,56 @@
 from django.core.files.images import ImageFile
 from django.test import TestCase
 
-from PIL import Image
 from io import BytesIO
+from sys import stdout
+from PIL import Image
 import os
 
-from apps.product.models import Product, ProductImage
+from apps.product.models import Product
 
 
 class ProductTestCase(TestCase):
-    def setUp(self):
-        img = self.create_test_image()
+    def save_img(self, size, form, color='RGB'):
+        img = Image.new(color, size)
+        img.save(f'media/tests/{size}_{form}_{color}.{form}', form)
+        img.close()
 
-        self.product = Product.objects.create(
-            name='test',
-            price=15,
-            thumbnail=img
-        )
-        self.image_obj = ProductImage.objects.create(
-            product=self.product,
-            image=img
-        )
+    def create_img(self):
+        sizes = ((100, 50), (256, 256), (1280, 720))
+        formats = ('png', 'jpeg')
+        colors = ('RGBA', 'RGB')
 
-    def test_product_creation(self):
-        self.assertIsNotNone(self.product)
-        self.assertIsNotNone(self.image)
-        
-    @staticmethod
-    def create_test_image():
-        img_name = 'luffygif.gif'
-        with Image.open(f'media/{img_name}') as image:
+        for s in range(len(sizes)):
+            for f in range(len(formats)):
+                if formats[f] == 'png':
+                    for c in range(len(colors)):
+                        self.save_img(sizes[s], formats[f], colors[c])
+                else:
+                    self.save_img(sizes[s], formats[f])
+
+    def get_img_file(self, name, form):
+        with Image.open(f'media/tests/{name}') as image:
             img_io = BytesIO()
-            image.save(img_io, format='gif')
-            image_file = ImageFile(img_io, name=img_name)
+            image.save(img_io, format=form)
+            image_file = ImageFile(img_io, name=name)
         return image_file
 
-    def test_resize_thumbnail(self):
-        product = Product.objects.get(name='test')
-        thumb = product.thumbnail
-
-        thumb_size = (thumb.width, thumb.height)
-        thumb_ext = os.path.splitext(thumb.name)[1]
-        thumb_name = thumb.name
-
-        self.assertIn('_Cmprssd', thumb_name)
-        self.assertLessEqual(thumb_size[0], 256)
-        self.assertEqual(thumb_ext, '.jpg')
-    
-
     def test_resize_image(self):
-        image = ProductImage.objects.get(
-            product__name=self.product
-        ).image
+        self.test_images = [name for name in os.listdir('media/tests/')]
 
-        img_size = (image.width, image.height)
-        img_ext = os.path.splitext(image.name)[1]
-        img_name = image.name
+        for img_name in self.test_images:
+            img_file = self.get_img_file(img_name, 'png')
 
-        self.assertIn('_Cmprssd', img_name)
-        self.assertLessEqual(img_size[0], 800)
-        self.assertEqual(img_ext, '.jpg')
+            product = Product.objects.create(
+                name=img_name, price=15, thumbnail=img_file
+            )
+            thumb = Product.objects.get(name=img_name).thumbnail
+
+            self.img_size = (thumb.width, thumb.height)
+            self.img_ext = os.path.splitext(thumb.name)[1]
+            self.img_name = thumb.name
+        
+            self.assertIn('_Cmprssd', self.img_name)
+            self.assertLessEqual(self.img_size[0], 256)
+            self.assertEqual(self.img_ext, '.jpg')
+    
