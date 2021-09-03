6cc0e9457bff0a83e8384d03c21fe0cdbfe291cb
imHireki <imhireki@gmail.com>
2021-09-03 18:46:29 -0300

    add user serializer

--- apps/account/models.py
+++ apps/account/models.py
@@ -1,53 +1,27 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+from utils.account import get_state_choices
 
-CASCADE = models.CASCADE
-STATE_CHOICES =  (
-    ('AC', 'Acre'),
-    ('AL', 'Alagoas'),
-    ('AP', 'Amapá'),
-    ('AM', 'Amazonas'),
-    ('BA', 'Bahia'),
-    ('CE', 'Ceará'),
-    ('DF', 'Distrito Federal'),
-    ('ES', 'Espírito Santo'),
-    ('GO', 'Goiás'),
-    ('MA', 'Maranhão'),
-    ('MT', 'Mato Grosso'),
-    ('MS', 'Mato Grosso do Sul'),
-    ('MG', 'Minas Gerais'),
-    ('PA', 'Pará'),
-    ('PB', 'Paraíba'),
-    ('PR', 'Paraná'),
-    ('PE', 'Pernambuco'),
-    ('PI', 'Piauí'),
-    ('RJ', 'Rio de Janeiro'),
-    ('RN', 'Rio Grande do Norte'),
-    ('RS', 'Rio Grande do Sul'),
-    ('RO', 'Rondônia'),
-    ('RR', 'Roraima'),
-    ('SC', 'Santa Catarina'),
-    ('SP', 'São Paulo'),
-    ('SE', 'Sergipe'),
-    ('TO', 'Tocantins'),
-)
 
 class User(AbstractUser):
-    """
-    cpf, birth_date
-    """
+    REQUIRED_FIELDS = ['email', 'cpf']
+    USERNAME_FIELD = 'username'
+
     cpf = models.CharField(max_length=11, null=True)
     birth_date = models.DateField(null=True)
 
-    REQUIRED_FIELDS = ['cpf', 'email',]
+    class Meta:
+        ordering = ('username',)
     
     def __str__(self):
         return self.username
-    
+
 
 class UserAddress(models.Model):
-    """ Many adresses for each user """
+    STATE_CHOICES = get_state_choices()
+    CASCADE = models.CASCADE
+
     user = models.ForeignKey(User, on_delete=CASCADE)
     name = models.CharField(max_length=32)
     adress = models.CharField(max_length=50)
@@ -62,6 +36,8 @@ class UserAddress(models.Model):
         choices = STATE_CHOICES,
     )
 
+    class Meta:
+        ordering = ('user',)
+    
     def __str__(self):
         f"{self.user}'s {self.name} address"
-    
