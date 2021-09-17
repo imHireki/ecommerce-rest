217f7e95618db659254b0e5794e2c40fee2b1c48
imHireki <imhireki@gmail.com>
2021-09-17 13:10:46 -0300

    tests: remove unused feature for testing user signup

--- tests/tests.py
+++ tests/tests.py
@@ -1,5 +1,4 @@
 from django.test import TestCase
-from rest_framework.response import Response
 import os
 
 from utils.product import get_img_file, create_img
@@ -34,26 +33,3 @@ class ProductTestCase(TestCase):
             self.assertIn('_Cmprssd', self.img_name)
             self.assertLessEqual(self.img_size[0], 256)
             self.assertEqual(self.img_ext, '.jpg')
-
-
-class AccountTestCase(TestCase):
-    def test_sign_up_serializer(self):
-        """
-        AssertionError: When a serializer is passed a `data` keyword argument 
-        you must call `.is_valid()` before attempting to access the serialized 
-        .data` representation. You should either call `.is_valid()` first,
-        or access `.initial_data` instead.
-        """
-        from apps.account.serializers import SignUpSerializer
-        from sys import stdout
-        
-        data = {
-            'username': 'test',
-            'password': 'pass',
-            'pass_confirm': 'pass',
-
-        }
-        serializer = SignUpSerializer(data=data)
-        serializer.is_valid(raise_exception=True)
-        
-        stdout.write(str(serializer.data))
